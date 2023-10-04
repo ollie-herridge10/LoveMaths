097e8618bb88ea790da1d6c096f7b796b5493589
ollie-herridge10 <141317500+ollie-herridge10@users.noreply.github.com>
2023-10-04 17:36:02 +0100

    some edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,13 +36,18 @@ function runGame(gameType) {
     }
 }
 
+/**
+ * Checks the answer agaist the first element in
+ * the returned calculateCorrectAnswer array
+ */
+
 function checkAnswer() {
 
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
-    if(isCorrect) {
+    if (isCorrect) {
         alert("Hey! You got it right! :D");
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
