80c09b3d91effc0be9dd7335c2d42c899fc444ba
ollie-herridge10 <141317500+ollie-herridge10@users.noreply.github.com>
2023-10-04 17:41:32 +0100

    corrected some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,6 @@ function runGame(gameType) {
  * Checks the answer agaist the first element in
  * the returned calculateCorrectAnswer array
  */
-
 function checkAnswer() {
 
     let userAnswer = parseInt(document.getElementById("answer-box").value);
@@ -54,18 +53,18 @@ function checkAnswer() {
     }
 
     runGame(calculatedAnswer[1]);
+
 }
 
 /**
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
-
 function calculateCorrectAnswer() {
 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = parseInt(document.getElementById('operator').innerText);
+    let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
@@ -73,7 +72,6 @@ function calculateCorrectAnswer() {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
-
 
 }
 
@@ -90,8 +88,7 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
-
-
+    
 }
 
 function displaySubtractQuestion() {
